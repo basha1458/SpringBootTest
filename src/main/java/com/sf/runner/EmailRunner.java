ac4081ee0f5fa31362b8b3abd3d106582b354cfd
DELL <farooq.shaik.atp3@gmail.com>
2024-09-03 14:04:33 +0530

    TN-6570-EmailSendinding

---
+++ src/main/java/com/sf/runner/EmailRunner.java
@@ -0,0 +1,13 @@
+package com.sf.runner;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class EmailRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+
+	}
+
+}
