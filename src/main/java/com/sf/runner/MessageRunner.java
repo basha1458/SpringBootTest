de8db150b2127968a2af761210d1146dde6ddac2
DELL <farooq.shaik.atp3@gmail.com>
2024-09-06 15:26:22 +0530

    WH-7656-update1

--- src/main/java/com/sf/runner/MessageRunner.java
+++ src/main/java/com/sf/runner/MessageRunner.java
@@ -1,15 +1,25 @@
 package com.sf.runner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 
-public class MessageRunner implements CommandLineRunner  {
+public class MessageRunner implements CommandLineRunner {
+	public static Logger log = LoggerFactory.getLogger(MessageRunner.class);
 
 	@Override
 	public void run(String... args) throws Exception {
-		int a=10;
-		System.out.println("MessageRunnerApp");
-		
+		try {
+			log.info("STARTED");
+			int a = 10;
+			System.out.println("MessageRunnerApp");
+			System.out.println("MessageRunnerApp");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		log.info("END");
+
 	}
-	
 
 }
