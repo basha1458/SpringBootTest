22e4c931ff65826179552edec87625e8a405684c
farooq shaik <farooq.shaik.atp3@gmail.com>
2025-05-12 23:39:50 +0530

    add syout statment

--- src/main/java/com/sf/runner/EmailRunner.java
+++ src/main/java/com/sf/runner/EmailRunner.java
@@ -6,6 +6,9 @@ public class EmailRunner implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		System.out.println("Welcome to Spring Boot App:");
+		System.out.println("Hello Extra code:");
+		
 		
 
 	}
